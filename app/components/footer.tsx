f12ab0501bce72a054b6ac55481a1918e4e24c5e
JDEV <qkrwoghd04@naver.com>
2024-09-11 15:33:16 +0900

    fist commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,16 +1,13 @@
-<p>It&apos;s a footer</p>
-
 import Link from "next/link"
 
 export default function Footer() {
   return (
     <>
       <footer className="body-font">
-        
         <div className="">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col ">
             <Link href="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="ml-3 text-xl">재홍's Portfolio</span>
+              <span className="ml-3 text-xl">재홍&apos;s Portfolio</span>
             </Link>
             <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
               <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,3 @@
-<p>It&apos;s a header</p>
 
 "use client"
 import Link from "next/link"
@@ -12,7 +11,7 @@ export default function Header() {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">재홍's Portfolio</span>
+          <span className="ml-3 text-xl">재홍&apos;s Portfolio</span>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
